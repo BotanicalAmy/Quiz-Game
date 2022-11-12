b26f46f94e437c53eca57a5e181f5c3fa2dc646f
BotanicalAmy <amyfolkestad@gmail.com>
2022-11-11 17:50:06 -0700

    Added extra quiz data (question_data_2)

--- data.py
+++ data.py
@@ -1,16 +1,199 @@
 question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
-             "you are free to take it home to eat.", "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
-     "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The HTML5 standard was published in 2014.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The first computer bug was formed by faulty wires.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "All program codes have to be compiled into an executable file in order to be run. This file can "
+                    "then be executed on any machine.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Linus Torvalds created Linux and Git.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The programming language 'Python' is based off a modified version of 'JavaScript'",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "AMD created the first consumer 64-bit processor.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "'HTML' stands for Hypertext Markup Language.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In most programming languages, the operator ++ is equivalent to the statement '+= 1'.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+            "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+            "True"
+        ]
+    }
+]
+
+question_data_2 = [
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The Pythagorean theorem states that the square of the hypotenuse is equal to the product of the "
+                    "squares of the other two sides.",
+        "correct_answer": "False"
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The proof for the Chinese Remainder Theorem used in Number Theory was NOT developed by its first "
+                    "publisher, Sun Tzu.",
+        "correct_answer": "True"
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A universal set, or a set that contains all sets, exists.",
+        "correct_answer": "False",
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "111,111,111 x 111,111,111 = 12,345,678,987,654,321",
+        "correct_answer": "True"
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "If you could fold a piece of paper in half 50 times, it's' thickness will be 3\/4th the distance from the Earth to the Sun.",
+        "correct_answer": "True"
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Zero factorial is equal to zero. ",
+        "correct_answer": "False"
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "A 'Millinillion' is a real number.",
+        "correct_answer": "True",
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "In Topology, the complement of an open set is a closed set.",
+        "correct_answer": "True"
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The set of all algebraic numbers is countable.",
+        "correct_answer": "True"
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The sum of any two odd integers is odd.",
+        "correct_answer": "False"
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "You can square root a negative number with an imaginary number 'i'.",
+        "correct_answer": "True"
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "An isosceles triangle has two sides of equal length as opposed to three.",
+        "correct_answer": "True"
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A scalene triangle has two sides of equal length.",
+        "correct_answer": "False"
+    }
 ]
